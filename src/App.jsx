5631f8dba6c5bd54ca0428e95aad293f22868034
Faranheit15 <ffaranm15@gmail.com>
2024-03-24 01:12:18 +0530

    ditching react for native HTML

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import { RouterProvider } from "react-router-dom";
-import router from "@/routes/index";
-import "./App.css";
-
-const App = () => {
-  return <RouterProvider router={router} />;
-};
-
-export default App;
--- src/components/DefaultGIF.jsx
+++
@@ -1,9 +0,0 @@
-const DefaultGIF = () => {
-  return (
-    <div>
-      <img src="https://media1.giphy.com/media/ymsh0cws7ROmko6VvM/giphy.gif?cid=ecf05e47ra2k3yoa8dmi9fec4gecd6zzfu1xchkqtsq9qpf4&ep=v1_stickers_search&rid=giphy.gif&ct=s" />
-    </div>
-  );
-};
-
-export default DefaultGIF;
